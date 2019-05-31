c444415a3df6794c359c4209b161ba3c014fb487
Miguel Alanis <anamelessdeath@gmail.com>
2019-05-31 10:40:30 -0500

    Chat creation service tested

--- src/services/ChatService.php
+++ src/services/ChatService.php
@@ -6,6 +6,8 @@ include_once  "lib/urabe/HasamiWrapper.php";
 class  ChatService  extends  HasamiWrapper
 {
 	const  TABLE_NAME  =  "chat";
+
+	private $userAccess;
 	/**
 	 * Initialize a new instance for the user table controller
 	 */
@@ -17,16 +19,33 @@ class  ChatService  extends  HasamiWrapper
 		$connector->init($conn);
 		parent::__construct(self::TABLE_NAME, $connector, "chatId");
 		$this->set_service_status("POST", ServiceStatus::LOGGED);
-		var_dump(get_access(1));
+		$this->userAccess = get_access();
+		if ($this->request_data->method == "PUT")
+			$this->request_data->body->insert_values->values->{"userId"} = $this->userAccess->userId;
 	}
-    /**
-     * Validates access for chat services
-     * @return boolean True if the validation access succeed
-     */
+	/**
+	 * Validates access for chat services
+	 * @return boolean True if the validation access succeed
+	 */
 	protected function validate_access()
 	{
 		session_start();
 		$token = $_SESSION["token"];
 		return !is_null($token);
+	}
+	/**
+	 * List all available chats for the current user
+	 *
+	 * @param WebServiceContent $data The web service content
+	 * @param Urabe $urabe The database manager
+	 * @return UrabeResponse The urabe response
+	 */
+	public function u_action_list($data, $urabe)
+	{
+		if ($this->userAccess->isAdmin)
+			$sql = "SELECT * FROM " . self::TABLE_NAME;
+		else
+			$sql = "SELECT * FROM " . self::TABLE_NAME . " WHERE userId=@1";
+		return $urabe->select($sql, array($this->userAccess->userId));
 	}
 }
