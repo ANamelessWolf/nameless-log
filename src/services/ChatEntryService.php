28fe361d6c35ef805e18f882f0eb44639f4cd165
ANamelessWolf/ <anamelessdeath@gmail.com>
2019-06-06 00:00:34 -0500

    Falta verificar la encriptación

--- src/services/ChatEntryService.php
+++ src/services/ChatEntryService.php
@@ -13,6 +13,10 @@ class  ChatEntryService  extends  HasamiWrapper
 	 */
 	private $chatId;
 	/**
+	 * Chat encrypter key
+	 */
+	private $chatKey;
+	/**
 	 * Stores the user access
 	 * @var UserAccess The user access
 	 */
@@ -33,10 +37,9 @@ class  ChatEntryService  extends  HasamiWrapper
 			$this->chatId = $this->request_data->get_variables["chatId"];
 		else
 			$this->chatId = null;
-		if ($this->request_data->method == "PUT")
-			$this->request_data->body->insert_values->values->creation_time = date("Y-m-d H:i:s");
+		$this->set_service_task("PUT", "add_entry");
+		$this->getChatKey();
 	}
-
 	/**
 	 * Validates access for chat entry
 	 * Only logged users are allowed
@@ -54,5 +57,48 @@ class  ChatEntryService  extends  HasamiWrapper
 			return $total > 0;
 		} else
 			return false;
+	}
+	/**
+	 * Adds a new entry to the log
+	 * @param WebServiceContent $data The web service content
+	 * @param Urabe $urabe The database manager
+	 * @return UrabeResponse The urabe response
+	 */
+	public function add_entry($data, $urabe)
+	{
+		$cat = new Caterpillar();
+		$data->body->insert_values->values->creation_time = date("Y-m-d H:i:s");
+		$data->body->insert_values->values->entry = $cat->encrypt($data->body->insert_values->values->entry);
+		return $urabe->insert(self::TABLE_NAME, $data->body->insert_values->values);
+	}
+	/**
+	 * Select all messages from the chat
+	 * @param WebServiceContent $data The web service content
+	 * @param Urabe $urabe The database manager
+	 * @return UrabeResponse The urabe response
+	 */
+	public function u_action_list($data, $urabe)
+	{
+		$cat = new Caterpillar();
+		$query_result =  $urabe->select_all(self::TABLE_NAME);
+		$result = $query_result->result;
+		for ($i = 0; $i < count($result); $i++)
+			//$result[$i]["entry"] = 
+			var_dump($cat->decrypt($result[$i]["entry"]));
+		$query_result->result = $result;
+		return $query_result;
+	}
+
+	/**
+	 * Gets the chat key
+	 */
+	private function getChatKey()
+	{
+		$urabe = $this->urabe->get_clone();
+		$fields = "SHA1( CONCAT(c.chatId, c.userId)) chatKey";
+		$leftJoin = "JOIN chat c ON c.chatId = cm.chatId";
+		$sql = "SELECT $fields FROM chat_members cm LEFT $leftJoin WHERE c.chatId = @1 AND cm.userId = @2";
+		$sql = $urabe->format_sql_place_holders($sql);
+		$this->chatKey = $urabe->select_one($sql, array($this->chatId, $this->userAccess->userId));
 	}
 }
